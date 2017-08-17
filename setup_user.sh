dbf03f34a25ed9a5a8891a9fbb6dfa6b916ddec8
dalseides <dalseides@users.noreply.github.com>
2017-08-17 13:39:12 -0500

    Added some adduser stuff.

--- setup_user.sh
+++ setup_user.sh
@@ -7,9 +7,7 @@ sudo mkdir /data
 wget -P /data http://images.freeimages.com/images/large-previews/8d7/trees-in-the-snow-1524996.jpg
 sudo sed -ie 's/wallpaper=.*/wallpaper=\/data\/trees-in-the-snow-1524996.jpg/' /usr/share/lxde/pcmanfm/LXDE.conf
 
-echo ----------------------
-echo $NEWUSER
-echo doodeedoodeedoo
-
 sudo groupadd console
-sudo usermod -a -G console vagrant
+sudo adduser --disabled-login --gecos ",,,," $NEWUSER
+echo "$NEWUSER:$NEWPASSWORD" | sudo chpasswd
+sudo usermod -a -G console $NEWUSER
