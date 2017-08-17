5ca12a54e4e21525b3d95ea825f3102b1bb77087
dalseides <dalseides@users.noreply.github.com>
2017-08-16 21:41:25 -0500

    Attempting (clumsily) to add a conf file.

--- provision.sh
+++ provision.sh
@@ -1,5 +1,7 @@
 #!/bin/sh
 
+source user.conf
+
 sudo apt-get update
 sudo apt-get install -y vim virtualbox-guest-dkms virtualbox-guest-utils virtualbox-guest-x11 
 sudo apt-get install -y xinit lxde hal #-common
@@ -15,10 +17,20 @@ sudo mkdir /data
 wget -P /data http://images.freeimages.com/images/large-previews/8d7/trees-in-the-snow-1524996.jpg
 sudo sed -ie 's/wallpaper=.*/wallpaper=\/data\/trees-in-the-snow-1524996.jpg/' /usr/share/lxde/pcmanfm/LXDE.conf
 
+cd /tmp/
+mkdir tmptmp
+git clone https://github.com/dalseides/vagrant-desktop.git
+source user.conf
+
+echo ----------------------
+echo $NEWUSER
+echo doodeedoodeedoo
+
 sudo groupadd console
 sudo usermod -a -G console vagrant
+
+sudo rm -rf /tmp/tmptmp
 
 sudo shutdown -r now
 #startlxde&
 #exec startxfce4&
-
